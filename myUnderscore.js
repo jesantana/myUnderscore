dafe286193bc367e6081ddc9ee0a7f56574b468e
Julio Santana (xe41271) <julio.santana.contractor@bbva.com>
2014-12-03 18:05:52 +0100

    reduce, some, find, where, findWhere, filter and reject implementations added

--- myUnderscore.js
+++ myUnderscore.js
@@ -82,9 +82,126 @@ function reverse(list){
 	return reduce(list,reverseSingleElement,[]);;
 }
 
+function reduceRightFunctional(list,funct,memo){
+	var currentIndex=list.length-1;	
+	function reduceRight(list,funct,memo){
+		if(currentIndex>=0){
+			var currentMemo=funct(list[currentIndex],memo);
+			--currentIndex;
+			return reduceRight(list,funct,currentMemo);
+		}
+		return memo;
+	}
+	
+	return reduceRight(list,funct,memo);
+}
+
 function reduceRight(list,funct,memo){
 	return reduce(reverse(list),funct,memo);
 }
 
+function some(list,predicate){
+	var currentIndex=0;
+	
+	function some(){
+		if(currentIndex<list.length){
+			return predicate(list[currentIndex])?list[currentIndex]:(++currentIndex,some());
+		}
+		return {};
+	}
+	
+	return some();
+}
 
-//Pensar en otro reduce
+function find(list,elem){
+	var currentIndex=0;
+	
+	
+	function findRecursive(list,elem){
+		if(currentIndex<list.length){
+			return list[currentIndex]==elem?currentIndex:(++currentIndex,findRecursive(list,elem));
+		}
+		return -1;
+	}
+	
+	return findRecursive(list,elem);
+}
+
+function findFunctional(list,elem){
+	var index=0;
+	
+	function testIfCurrentEqualsElement(current,memo){
+		if(memo===-1){
+			var result=index++;
+			return current===elem?result:memo;
+		}
+		return memo;
+	}
+	
+	return reduce(list,testIfCurrentEqualsElement,-1);
+}
+
+
+function where(list,properties){
+
+	function addObjectToListIfPropertiesPresent(currentObj,memo){
+		for(var props in properties){
+			if(currentObj[props]===undefined || currentObj[props]!==properties[props]){
+				return memo;
+			}
+		}
+		
+		return memo.concat(currentObj);
+	}
+	
+	return reduce(list,addObjectToListIfPropertiesPresent,[]);
+}
+
+
+function findWhere(list,properties){
+
+	function testIfObjectHaveAllProperties(currentObj){
+		for(var props in properties){
+			if(currentObj[props]===undefined || currentObj[props]!==properties[props]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	return some(list,testIfObjectHaveAllProperties);
+}
+
+function filter(list,predicate){
+	
+	function filter(element,memo){
+		if(predicate(element)){
+			return memo.concat(element)
+		}
+		else{
+			return memo
+		}
+	}
+	
+	return reduce(list,filter,[]);
+}
+
+function reject(list,predicate){
+	
+	function reject(element,memo){
+		return predicate(element)?memo:memo.concat(element);
+	}
+
+	return reduce(list,reject,[]);
+}
+
+
+function rejectFilterBased(list,predicate){
+	
+	function inversePredicate(element){
+		return !predicate(element);
+	}
+
+	return filter(list,inversePredicate);
+}
+
--- test.js
+++ test.js
@@ -5,6 +5,7 @@ console.log('Hello from Test');
 //console.log(reduce([10,2,3,5,94,5,7], function(current,memo){return memo>current?memo:current;},0));
 //console.log(reverse([10,2,3,5,94,5,7]));
 var valueToCompare=7;
+/*
 function firstGreaterThan(current,memo){
 	var result=0;
 	
@@ -17,6 +18,25 @@ function firstGreaterThan(current,memo){
 	
 	return result;
 }
+*/
+//console.log(reduceRight([2,3,10,5,94,5,7],firstGreaterThan,0));
+//console.log(reduceRightFunctional([2,3,10,5,94,5,7],firstGreaterThan,0));
 
-console.log(reduceRight([2,3,10,5,94,5,7],firstGreaterThan,0));
-console.log(reduce([2,3,10,5,94,5,7],firstGreaterThan,0));
+/*console.log(findRecursive([2,3,10,5,94,5,7],5));
+console.log(findFunctional([2,3,10,5,94,5,7],5));
+
+console.log(some([2,3,10,5,94,5,7],function(element){return element>6}))*/
+
+/*var objArray=[{a:'a',b:'b',c:'c'},
+					{x:'a',y:'b',c:'c'},
+					{a:'a',y:'b',c:'c'},
+					{y:'a',b:'b',c:'c'}
+					];
+var comparingObje={c:'c',y:'a'};
+
+console.log(where(objArray,comparingObje));
+console.log(findWhere(objArray,comparingObje));
+*/
+console.log(filter([1,5,6,2,9,3],function(element){return element%2==0;}));
+console.log(reject([1,5,6,2,9,3],function(element){return element%2==0;}));
+console.log(rejectFilterBased([1,5,6,2,9,3],function(element){return element%2==0;}));
